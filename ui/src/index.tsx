0bce4d392737232267ca2a949940cbce2e277b7b
Andrew Mehta <andrew.mehta@lattice.com>
2025-04-10 09:35:46 -0600

    update release script with vite

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+// I get an error on build when I have this import, but I get eslint errors in the editor when I don't... probably fixable. Worth investigating
+// import React from 'react';
 import LatticeTab, { Application } from './latticeTab';
 
 // Register the extensions 1 at a time
